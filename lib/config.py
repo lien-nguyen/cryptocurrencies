4ec98d86d4b94275ff2e1ee9a7f2e84b3ea9e6ae
lien-nguyen <thi.bich.l.nguyen@gmail.com>
2021-03-15 16:41:28 +0100

    add config.py to gitignore

--- lib/config.py
+++ lib/config.py
@@ -2,7 +2,7 @@
 api_tokens = '87c8f939-6ef3-44f0-863c-c07a7d3df545'
 
 # Path to input data folder
-# PATH = "/Users/lien.nguyen/Documents/samsungzeil/database_project/data"
+# PATH = "/Users/lien.nguyen/path-to-input-data"
 
 # Path to ouput data folder
-#OUTPUT = "/Users/lien.nguyen/Documents/samsungzeil/database_project/summary_data"
+#OUTPUT = "/Users/lien.nguyen/path-to-output"
