0d33bdb5f15bfb4b8e9377578607424e0418dc66
lien-nguyen <thi.bich.l.nguyen@gmail.com>
2021-03-15 16:38:07 +0100

    Add new file lib/colors & modify lib/config.py (instead of credentials.py)

---
+++ lib/colors.py
@@ -0,0 +1,17 @@
+# color codes
+
+CY = '\033[36m' # cyan
+WH = '\033[0m' # white
+RE = '\033[31m' # red
+LG = '\033[37m' # lightgrey
+DG = '\033[90m' # darkgrey
+LR = '\033[91m' # lightred
+GR = '\033[32m' # green
+OR = '\033[33m' # orange
+LG ='\033[92m' # lightgreen
+YE = '\033[93m' # yellow
+LB = '\033[94m' # lightblue
+BL = '\033[34m' # blue
+PU = '\033[35m' # purple
+PI = '\033[95m' # pink
+LC = '\033[96m' # lightcyan
---
+++ lib/config.py
@@ -0,0 +1,8 @@
+# Api key obtained on https://pro.coinmarketcap.com/
+api_tokens = '87c8f939-6ef3-44f0-863c-c07a7d3df545'
+
+# Path to input data folder
+# PATH = "/Users/lien.nguyen/Documents/samsungzeil/database_project/data"
+
+# Path to ouput data folder
+#OUTPUT = "/Users/lien.nguyen/Documents/samsungzeil/database_project/summary_data"
