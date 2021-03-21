4d8141869c6697f8e42d47985527ceb9085ac3ab
lien-nguyen <thi.bich.l.nguyen@gmail.com>
2021-03-21 14:25:21 +0100

    Modify gitignore

--- lib/config.py
+++
@@ -1,8 +0,0 @@
-# Api key obtained on https://pro.coinmarketcap.com/
-api_tokens = '87c8f939-6ef3-44f0-863c-c07a7d3df545'
-
-# Path to input data folder
-# PATH = "/Users/lien.nguyen/path-to-input-data"
-
-# Path to ouput data folder
-#OUTPUT = "/Users/lien.nguyen/path-to-output"
